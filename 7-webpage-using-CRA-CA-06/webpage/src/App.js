807773cb4ea59a0175ecaf78c0dfefd563a88c36
jibanranjan <jdebata427@gmail.com>
2023-05-31 11:45:37 +0530

    hootsuite webpage cra

---
+++ 7-webpage-using-CRA-CA-06/webpage/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Mid from './Components/Mid';
+import Last from './Components/Last';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Mid />
+      <Last />
+    </div>
+  );
+}
+
+export default App;
